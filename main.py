39cfac0c6e23a236e5f15ae06d297af852e0af2f
Tim Lubes <tim.lubes@gmail.com>
2020-06-20 18:50:53 -0700

    fixes for single layer smts and different values for same components

--- main.py
+++ main.py
@@ -36,55 +36,61 @@ partDict[botOfBoard] = []
 topData = sys.argv[1].split(layerSeperator)[0]
 bottomData = sys.argv[1].split(layerSeperator)[1]
 
-for part in topData.split(partSeperator):
-    topPart = {}
-    topPart['name'] = part.split(propertySeperator)[0]
-    topPart['x'] = part.split(propertySeperator)[1] + 'mm'
-    topPart['y'] = part.split(propertySeperator)[2] + 'mm'
-    topPart['angle'] = part.split(propertySeperator)[3].replace('_', '')
-    topPart['value'] = part.split(propertySeperator)[4]
-    topPart['package'] = part.split(propertySeperator)[5]
-    topPart['library'] = part.split(propertySeperator)[6]
-    topPart['device'] = part.split(propertySeperator)[7]
-    partDict[topOfBoard].append(topPart)
+logging.debug(sys.argv[1])
 
-for part in bottomData.split(partSeperator):
-    bottomPart = {}
-    bottomPart['name'] = part.split(propertySeperator)[0]
-    bottomPart['x'] = part.split(propertySeperator)[1] + 'mm'
-    bottomPart['y'] = part.split(propertySeperator)[2] + 'mm'
-    bottomPart['angle'] = part.split(propertySeperator)[3].replace('_', '')
-    bottomPart['value'] = part.split(propertySeperator)[4]
-    bottomPart['package'] = part.split(propertySeperator)[5]
-    bottomPart['library'] = part.split(propertySeperator)[6]
-    bottomPart['device'] = part.split(propertySeperator)[7]
-    partDict[botOfBoard].append(bottomPart)
+
+if topData:
+    for part in topData.split(partSeperator):
+        topPart = {}
+        topPart['name'] = part.split(propertySeperator)[0]
+        topPart['x'] = part.split(propertySeperator)[1] + 'mm'
+        topPart['y'] = part.split(propertySeperator)[2] + 'mm'
+        topPart['angle'] = part.split(propertySeperator)[3].replace('_', '')
+        topPart['value'] = part.split(propertySeperator)[4]
+        topPart['package'] = part.split(propertySeperator)[5]
+        topPart['library'] = part.split(propertySeperator)[6]
+        topPart['device'] = part.split(propertySeperator)[7]
+        partDict[topOfBoard].append(topPart)
+
+if bottomData:
+    for part in bottomData.split(partSeperator):
+        bottomPart = {}
+        bottomPart['name'] = part.split(propertySeperator)[0]
+        bottomPart['x'] = part.split(propertySeperator)[1] + 'mm'
+        bottomPart['y'] = part.split(propertySeperator)[2] + 'mm'
+        bottomPart['angle'] = part.split(propertySeperator)[3].replace('_', '')
+        bottomPart['value'] = part.split(propertySeperator)[4]
+        bottomPart['package'] = part.split(propertySeperator)[5]
+        bottomPart['library'] = part.split(propertySeperator)[6]
+        bottomPart['device'] = part.split(propertySeperator)[7]
+        partDict[botOfBoard].append(bottomPart)
      
 # prepare bom file list
 
 bomList = []
 
-for part in partDict[topOfBoard]:
-    foundPart = False
-    for mat in bomList:
-        logging.debug(mat["library"],part["library"],mat["device"],part["device"])
-        if mat["library"] == part["library"] and mat["device"] == part["device"]:
-            logging.debug('here')
-            mat['designator'] += ("," + part['name'])
-            foundPart = True
-            break
-    if foundPart is False:
-        bomList.append({'designator': part['name'], 'comment': part['value'], 'footprint': part['package'], 'library': part['library'], 'device': part['device']})
+if topData:
+    for part in partDict[topOfBoard]:
+        foundPart = False
+        for mat in bomList:
+            if mat["library"] == part["library"] and mat["device"] == part["device"] and mat["comment"] == part["value"]:
+                logging.debug('here')
+                mat['designator'] += ("," + part['name'])
+                foundPart = True
+                break
+        if foundPart is False:
+            bomList.append({'designator': part['name'], 'comment': part['value'], 'footprint': part['package'], 'library': part['library'], 'device': part['device']})
 
-for part in partDict[botOfBoard]:
-    foundPart = False
-    for mat in bomList:
-        if mat["library"] == part["library"] and mat["device"] == part["device"]:
-            mat['designator'] += ("," + part['name'])
-            foundPart = True
-            break
-    if foundPart is False:
-        bomList.append({'designator': part['name'], 'comment': part['value'], 'footprint': part['package'], 'library': part['library'], 'device': part['device']})        
+if bottomData:
+    for part in partDict[botOfBoard]:
+        foundPart = False
+        for mat in bomList:
+            if mat["library"] == part["library"] and mat["device"] == part["device"] and mat["comment"] == part["value"]:
+                mat['designator'] += ("," + part['name'])
+                foundPart = True
+                break
+        if foundPart is False:
+            bomList.append({'designator': part['name'], 'comment': part['value'], 'footprint': part['package'], 'library': part['library'], 'device': part['device']})        
 
 # writing pnp file
 
@@ -92,12 +98,14 @@ with open(pnpFile, mode='w') as file:
     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     # this is the header row of the csv table
-    writer.writerow(['Designator', 'Mid X', 'Mid Y', 'Layer', 'Rotation'])    
-    for part in partDict[topOfBoard]:
-        writer.writerow([part['name'],part['x'], part['y'], 'Top', part['angle']])
+    writer.writerow(['Designator', 'Mid X', 'Mid Y', 'Layer', 'Rotation'])
+    if topData:    
+        for part in partDict[topOfBoard]:
+            writer.writerow([part['name'],part['x'], part['y'], 'Top', part['angle']])
 
-    for part in partDict[botOfBoard]:
-        writer.writerow([part['name'],part['x'], part['y'], 'Bottom', part['angle']])         
+    if bottomData:  
+        for part in partDict[botOfBoard]:
+            writer.writerow([part['name'],part['x'], part['y'], 'Bottom', part['angle']])         
 
 # writing bom file
 
